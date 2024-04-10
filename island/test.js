45ef3b0ba9d32f8b85c505f49732c0cd13e03925
ljh255 <jinhai@carbonstop.net>
2024-04-10 14:59:09 +0800

    fix:修改.gitIgnore

--- island/test.js
+++ island/test.js
@@ -1,4 +1,10 @@
-function test() {
-  console.log("test");
-}
-test();
+const Koa = require("koa");
+const books = require("./api/vi/books");
+
+const app = new Koa();
+
+app.use(books.routes());
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
